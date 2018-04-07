9bd63db65006593e7742037cc2c47373497bc79e
Murilo <murilo.arantess@gmail.com>
2018-04-07 04:46:02 -0300

    intCounter created

---
+++ intCounter.go
@@ -0,0 +1,39 @@
+package counter
+
+import (
+	"sync"
+)
+
+const (
+	counterCollection = "counters"
+	keyField          = "key"
+	durationField     = "duration"
+)
+
+var (
+	db               = ""
+	persistInterval  = 10
+	persistenceQueue persistanceModel
+)
+
+//Inc int of key
+func Inc(key string, inc int) {
+	persistenceQueue.inc(key, inc)
+}
+
+type persistanceModel struct {
+	mapDurationToPersist map[string]int
+	mux                  sync.Mutex
+}
+
+func (p *persistanceModel) inc(key string, inc int) {
+	p.mux.Lock()
+	val, ok := p.mapDurationToPersist[key]
+	if ok {
+		val += inc
+		p.mapDurationToPersist[key] = val
+	} else {
+		p.mapDurationToPersist[key] = inc
+	}
+	p.mux.Unlock()
+}
