c2e1c10d039c143a1cbe74938bd0abc8375006a5
Murilo <murilo.arantess@gmail.com>
2018-05-03 02:43:13 -0300

    Split tests

--- counter_test.go
+++ counter_test.go
@@ -21,80 +21,80 @@ var (
 	zeroIntTest      int
 )
 
-func TestValNotStopped(t *testing.T) {
-	dropDataBase(dbTest)
+func TestIncAndVal(t *testing.T) {
 	sumTime := sumQtdeTimeToTest()
 	sumInt := sumQtdeIntToTest()
-	ctime.Enable()
-	cint.Enable()
-	counter.Start(dbTest, 10)
-	for i := 0; i < qtdeTest; i++ {
-		ctime.Inc("k1", time.Duration(i))
-		ctime.Inc("k2", time.Duration(i))
-		ctime.Inc("k3", time.Duration(i))
-		cint.Inc("k1", i)
-		cint.Inc("k2", i)
-		cint.Inc("k3", i)
-	}
-	time.Sleep(90 * time.Millisecond) // Waiting to increment all values
+	startDefaultValues()
+	time.Sleep(80 * time.Millisecond) // Waiting to increment all values
 	if sumTime != ctime.Val("k1") || sumTime != ctime.Val("k2") || sumTime != ctime.Val("k3") ||
 		sumInt != cint.Val("k1") || sumInt != cint.Val("k2") || sumInt != cint.Val("k3") {
 		t.FailNow()
 	}
-	log.Println(t.Name(), "1 ok. Should return correct values")
 	counter.Stop()
 }
 
-func TestIncAndGetAndStop(t *testing.T) {
-	dropDataBase(dbTest)
+func TestIncAndValStop(t *testing.T) {
 	sumTime := sumQtdeTimeToTest()
 	sumInt := sumQtdeIntToTest()
-	ctime.Enable()
-	cint.Enable()
-	counter.Start(dbTest, 10)
-	for i := 0; i < qtdeTest; i++ {
-		ctime.Inc("k1", time.Duration(i))
-		ctime.Inc("k2", time.Duration(i))
-		ctime.Inc("k3", time.Duration(i))
-		cint.Inc("k1", i)
-		cint.Inc("k2", i)
-		cint.Inc("k3", i)
-	}
+	startDefaultValues()
 	counter.Stop() // persist
 	if sumTime != ctime.Val("k1") || sumTime != ctime.Val("k2") || sumTime != ctime.Val("k3") ||
 		sumInt != cint.Val("k1") || sumInt != cint.Val("k2") || sumInt != cint.Val("k3") {
 		t.FailNow()
 	}
-	log.Println(t.Name(), "1 ok. Should return correct values")
+}
 
+func TestIncAndValAndRestart(t *testing.T) {
+	sumTime := sumQtdeTimeToTest()
+	sumInt := sumQtdeIntToTest()
+	startDefaultValues()
+	counter.Stop() // persist
 	counter.Start(dbTest, 10)
 	if sumTime != ctime.Val("k1") || sumTime != ctime.Val("k2") || sumTime != ctime.Val("k3") ||
 		sumInt != cint.Val("k1") || sumInt != cint.Val("k2") || sumInt != cint.Val("k3") {
 		t.FailNow()
 	}
-	log.Println(t.Name(), "2 ok. Should continue works")
+}
 
+func TestIncAndValAndRestartAndInc(t *testing.T) {
+	sumTime := sumQtdeTimeToTest()
+	sumInt := sumQtdeIntToTest()
+	startDefaultValues()
+	counter.Stop() // persist
+	counter.Start(dbTest, 10)
 	for i := 0; i < qtdeTest; i++ {
 		ctime.Inc("k1", time.Duration(i))
 		cint.Inc("k1", i)
 	}
 	ctime.Inc("k2", sumTime)
 	cint.Inc("k2", sumInt)
-	time.Sleep(90 * time.Millisecond) // Waiting to increment all values
+	time.Sleep(80 * time.Millisecond) // Waiting to increment all values
 	if sumTime*2 != ctime.Val("k1") || sumTime*2 != ctime.Val("k2") || sumInt*2 != cint.Val("k1") || sumInt*2 != cint.Val("k2") {
 		t.FailNow()
 	}
-	log.Println(t.Name(), "3 ok. Should return correct values after restart and not persist")
+	counter.Stop()
+}
 
+func TestIncAndValAndRestartAndIncAndStop(t *testing.T) {
+	sumTime := sumQtdeTimeToTest()
+	sumInt := sumQtdeIntToTest()
+	startDefaultValues()
+	counter.Stop() // persist
+	counter.Start(dbTest, 10)
+	for i := 0; i < qtdeTest; i++ {
+		ctime.Inc("k1", time.Duration(i))
+		cint.Inc("k1", i)
+	}
+	ctime.Inc("k2", sumTime)
+	cint.Inc("k2", sumInt)
 	counter.Stop()
 	if sumTime*2 != ctime.Val("k1") || sumTime*2 != ctime.Val("k2") || sumTime != ctime.Val("k3") ||
 		sumInt*2 != cint.Val("k1") || sumInt*2 != cint.Val("k2") || sumInt != cint.Val("k3") {
 		t.FailNow()
 	}
-	log.Println(t.Name(), "4 ok. Should persist and return correct values after restart and persist")
 }
 
-func TestIncAndClear(t *testing.T) {
+func TestIncAndValAndClear(t *testing.T) {
 	dropDataBase(dbTest)
 	ctime.Enable()
 	cint.Enable()
@@ -103,29 +103,48 @@ func TestIncAndClear(t *testing.T) {
 	ctime.Inc("k1", sumQtdeTimeToTest())
 	cint.Inc("k1", sumQtdeIntToTest())
 	time.Sleep(60 * time.Millisecond)
-	if sumQtdeTimeToTest() != ctime.Val("k1") || sumQtdeIntToTest() != cint.Val("k1") {
-		t.FailNow()
-	}
-	log.Println(t.Name(), "1 ok. Increment should works")
-
 	ctime.Clear("k1")
 	cint.Clear("k1")
 	if cint.Val("k1") != zeroIntTest || ctime.Val("k1") != zeroDurationTest {
 		t.FailNow()
 	}
-	log.Println(t.Name(), "2 ok. Clear and should return correct values")
+	counter.Stop()
+}
 
+func TestIncAndValAndClearAndStop(t *testing.T) {
+	dropDataBase(dbTest)
+	ctime.Enable()
+	cint.Enable()
+	counter.Start(dbTest, 10)
+
+	ctime.Inc("k1", sumQtdeTimeToTest())
+	cint.Inc("k1", sumQtdeIntToTest())
+	time.Sleep(60 * time.Millisecond)
+	ctime.Clear("k1")
+	cint.Clear("k1")
 	counter.Stop()
 	if cint.Val("k1") != zeroIntTest || ctime.Val("k1") != zeroDurationTest {
 		t.FailNow()
 	}
-	log.Println(t.Name(), "3 ok. Clear, persist and should return correct values")
+	counter.Stop()
+}
 
+func TestIncAndValAndClearAndRestart(t *testing.T) {
+	dropDataBase(dbTest)
+	ctime.Enable()
+	cint.Enable()
+	counter.Start(dbTest, 10)
+
+	ctime.Inc("k1", sumQtdeTimeToTest())
+	cint.Inc("k1", sumQtdeIntToTest())
+	time.Sleep(60 * time.Millisecond)
+	ctime.Clear("k1")
+	cint.Clear("k1")
+	counter.Stop()
 	counter.Start(dbTest, 10)
 	if cint.Val("k1") != zeroIntTest || ctime.Val("k1") != zeroDurationTest {
 		t.FailNow()
 	}
-	log.Println(t.Name(), "4 ok. Clear, persist and restart should return correct values")
 	counter.Stop()
 }
 
@@ -147,7 +166,7 @@ func TestRestartShouldClearAll(t *testing.T) {
 	if ctime.Val("k1") != zeroDurationTest || cint.Val("k1") != zeroIntTest {
 		t.FailNow()
 	}
-	log.Println(t.Name(), "2 ok. Increment and restart with other db should zero the values")
+	log.Println(t.Name(), "2 ok. Increment and restart with other db should clear the values")
 
 	ctime.Inc("k1", sumQtdeTimeToTest())
 	cint.Inc("k1", sumQtdeIntToTest())
@@ -155,8 +174,7 @@ func TestRestartShouldClearAll(t *testing.T) {
 	if sumQtdeTimeToTest() != ctime.Val("k1") || sumQtdeIntToTest() != cint.Val("k1") {
 		t.FailNow()
 	}
-	log.Println(t.Name(), "3 ok. Increment and restart and stop with other db should zero the values")
-
+	log.Println(t.Name(), "3 ok. Increment and restart and stop with other db should clear the values")
 }
 
 func sumQtdeTimeToTest() time.Duration {
@@ -169,6 +187,21 @@ func sumQtdeIntToTest() int {
 		sum += i
 	}
 	return sum
+}
+
+func startDefaultValues() {
+	dropDataBase(dbTest)
+	ctime.Enable()
+	cint.Enable()
+	counter.Start(dbTest, 10)
+	for i := 0; i < qtdeTest; i++ {
+		ctime.Inc("k1", time.Duration(i))
+		ctime.Inc("k2", time.Duration(i))
+		ctime.Inc("k3", time.Duration(i))
+		cint.Inc("k1", i)
+		cint.Inc("k2", i)
+		cint.Inc("k3", i)
+	}
 }
 
 func dropDataBase(dbs ...string) {
